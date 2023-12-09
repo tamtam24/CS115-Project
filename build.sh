a85ebf9e500b5baedd3eb6f12b89304e923e9640
tamtam24 <danglethanhtam2004@gmail.com>
2023-12-09 16:08:51 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 # exit on error
+# set -o errexit
+
+# pip install --upgrade pip
+# pip install -r requirements.txt
+
+# poetry install
+
+# python main.py collectstatic
+# python main.py migrate
+
+
+
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-pip install --upgrade pip
-pip install -r requirements.txt
 
 poetry install
-
-python main.py collectstatic
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
+python main.py collectstatic --no-input
 python main.py migrate
